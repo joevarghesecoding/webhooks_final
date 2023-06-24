bd20f8b86f082f48bede5a45fd7df4c46de4f620
joevarghesecoding <joevarghesecoding@gmail.com>
2023-06-24 14:19:58 -0400

    using noop to keepalive instead

--- webhook ctdi/emailReader/emailReader.js
+++ webhook ctdi/emailReader/emailReader.js
@@ -1,6 +1,5 @@
 const Imap = require('imap');
 const { simpleParser } = require('mailparser');
-const axios = require("axios").default;
 require('dotenv').config({path: '../.env'});
 
 const imap = new Imap({
@@ -13,8 +12,51 @@ const imap = new Imap({
         rejectUnauthorized: false
       },
     // requireTLS: true,
-    //debug: console.log
+    debug: console.log,
+    keepalive: {
+        interval: 5000,
+        idleInterval: 6000,
+        forceNoop: true
+    }
 });
+
+const readEmails = (resolve, reject) => {
+    imap.search(['UNSEEN', ['SINCE', new Date()]],  (err, results) => {
+        if(err) {
+            console.log('error at search');
+            throw err;
+        }
+
+        if(!results || !results.length) {
+            console.log('No unread emails');
+            //imap.end();
+            return;
+        }
+        let f = imap.fetch(results, { bodies: ''});
+        f.once('message',  (msg, seqno) => {
+            msg.once('body', async (stream, info) => {
+                parsed = await simpleParser(stream);
+                //console.log("parsed inside " +parsed.text);
+                resolve(parsed);
+            });
+            msg.once('attributes', attrs => {
+                const {uid} = attrs;
+                imap.addFlags(uid, ['\\Seen'], () => {
+                    console.log('Marked as read!');
+                })
+            })
+        });
+        f.once('error' , (error) => {
+            console.log('fetch error ' + error);
+            return reject(error);
+        });
+        // f.once('end', () => {
+        //     console.log('Done fetching all messages');
+        //     imap.end();
+        //     return;
+        // });
+    });
+}
 
 const getEmails = () => {
     return new Promise((resolve, reject) => {
@@ -27,69 +69,26 @@ const getEmails = () => {
                 if(err) {
                     console.log('error at ready ' + err);
                 }
-                imap.search(['UNSEEN', ['SINCE', new Date()]],  (err, results) => {
-                    if(err) {
-                        console.log('error at search');
-                        throw err;
-                    }
-
-                    if(!results || !results.length) {
-                        console.log('No unread emails');
-                        imap.end();
-                        return;
-                    }
-                    let f = imap.fetch(results, { bodies: ''});
-                    f.once('message',  (msg, seqno) => {
-                        msg.once('body', async (stream, info) => {
-                            parsed = await simpleParser(stream);
-                            //console.log("parsed inside " +parsed.text);
-                            resolve(parsed);
-                        });
-                        msg.once('attributes', attrs => {
-                            const {uid} = attrs;
-                            imap.addFlags(uid, ['\\Seen'], () => {
-                                console.log('Marked as read!');
-                            })
-                        })
-                    });
-                    f.once('error' , (error) => {
-                        console.log('fetch error ' + error);
-                        return reject(error);
-                    });
-                    f.once('end', () => {
-                        console.log('Done fetching all messages');
-                        imap.end();
-                        return;
-                    });
-                });
+                startMessageLoop(resolve, reject);
             });
             imap.once('error', err => {
                 console.log(err);
             });
-            imap.once('end', ()=> {
-                imap.removeAllListeners();
-                console.log('Listener ended');
-                imap.end();
+            // imap.once('end', ()=> {
+            //     imap.removeAllListeners();
+            //     console.log('Listener ended');
+            //     imap.end();
 
-            })
+            // })
         });
-        })
+        console.log('Mail service ended');
+    })
 }
 
-function startMessageLoop() {
+function startMessageLoop(resolve, reject) {
     const checkInterval = 5000;
     setInterval(() => {
-        getEmails().then((content) => {
-            axios.post(process.env.TEAMS_WEBHOOK_URL, content)
-            .then((teamsResponse) => {
-              console.log("SUCCESS");
-            })
-            .catch((err) => {
-              console.log(`Error sending to teams: ${err}`);
-              console.log(err.response.status);
-              console.log(err.response.data);
-            })
-          });
+        readEmails(resolve, reject)
     }, checkInterval);
 }
 
--- webhook ctdi/index.js
+++ webhook ctdi/index.js
@@ -4,11 +4,22 @@ const emailReader = require('./emailReader/emailReader')
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 3778;
 
 app.use(express.json());
 
-emailReader.startMessageLoop();
+//emailReader.startMessageLoop();
+emailReader.getEmails().then((content) => {
+  axios.post(process.env.TEAMS_WEBHOOK_URL, content)
+  .then((teamsResponse) => {
+    console.log("SUCCESS");
+  })
+  .catch((err) => {
+    console.log(`Error sending to teams: ${err}`);
+    console.log(err.response.status);
+    console.log(err.response.data);
+  })
+});
 
 app.listen(port, () =>
   console.log(`Test app listening at http://localhost:${port}`)
