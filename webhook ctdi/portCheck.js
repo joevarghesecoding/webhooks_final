8fb005eb6ce9bc0b3bc3cac13b805959d27193ff
joevarghesecoding <joevarghesecoding@gmail.com>
2023-06-24 17:02:36 -0400

    portCheck

---
+++ webhook ctdi/portCheck.js
@@ -0,0 +1,67 @@
+const net = require('net');
+const { exec } = require('child_process');
+
+const path = 'index.js';
+const port = 3778;
+
+function checkPortInUse(port) {
+    return new Promise((resolve) => {
+        const server = net.createServer()
+        .once('error', () => {
+            server.close();
+        })
+        .once('listening', () => {
+            server.close();
+            resolve(false);
+        })
+        .listen(port, 'localhost');
+    })
+}
+
+function triggerScript(){
+    console.log(`Port ${port} is available. Triggering the script...`);
+    exec(`node ${path}`, (error, stdout, stderr) => {
+        if(error) {
+            if(error.errno === -48){
+                console.log('Server checked and is in USE');
+            } else {
+                //console.log(`Error Executing. ${error.message}`);
+            }
+            return;
+        }
+
+        if(stderr){
+            console.log(`Script error. ${stderr}`);
+            return;
+        }
+
+        console.log(`${stdout}`);
+    })
+}
+
+function startPort(port) {
+    checkPortInUse(port)
+            .then((using) => {
+                if(using){
+                    console.log(`Port ${port} is already in use`);
+                } else {
+                    triggerScript();
+                }
+            })
+            .catch((err) => {
+                console.log(`Error occuring while checking port: ${port}`, err);
+            })
+}
+
+
+function checkLoop(port) {
+    const checkInterval = 6000;
+    startPort(port);
+    setInterval(() => {
+        console.log('checking port activity...');
+        startPort(port);
+    }, checkInterval);
+}
+
+checkLoop(port);
+
